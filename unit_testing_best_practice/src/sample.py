7d6a1a1b300a3738fe3f40f492b3845a74f8e0f1
JohanFR198 <39226062+JohanFR198@users.noreply.github.com>
2026-02-08 17:57:49 -0600

    Feat: Functions for PWD, USR_NM and Email

--- unit_testing_best_practice/src/sample.py
+++ unit_testing_best_practice/src/sample.py
@@ -1,3 +1,18 @@
 
 def func(x):
     return x + 2
+
+
+def get_username():
+    user_name = input("""Enter your username: """)
+    return user_name
+
+
+def get_password():
+    password = input("""Enter your password: """)
+    return password
+
+
+def get_email():
+    email = input("""Enter your email: """)
+    return email
