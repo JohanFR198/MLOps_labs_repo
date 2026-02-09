23317600c8db9b39794cc749a731d68e1dca4df5
JohanFR198 <39226062+JohanFR198@users.noreply.github.com>
2026-02-08 18:10:05 -0600

    Write test for TDD

---
+++ unit_testing_best_practice/test/test_tdd.py
@@ -0,0 +1,8 @@
+def validate_isprime():
+    assert is_prime(2)
+    assert is_prime(3)
+    assert not is_prime(4)
+    assert is_prime(5)
+    assert not is_prime(12)
+    assert not is_prime(25)
+    
